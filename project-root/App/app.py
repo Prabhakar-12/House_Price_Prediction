97d04ab11adcff7730037e336d1f4c37a5cc0648
Prabhakar <PrabhakarKondakalla9@gmail.com>
2025-08-18 23:45:54 +0530

    Added Pkl file

---
+++ project-root/App/app.py
@@ -0,0 +1,8 @@
+from flask import Flask
+app=Flask(__name__)
+
+@app.route('/',methods=['GET'])
+def hello_world():
+    return "Hello World Prabhakar"
+if __name__=='__main__':
+    app.run(port=3000,debug=True)
