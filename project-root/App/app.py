9f2227800d8c794852fc1df9ba65208e0b009ca1
Prabhakar <prabhakarkondakalla9@gmail.com>
2025-10-26 11:09:24 +0530

    //

--- project-root/App/app.py
+++ project-root/App/app.py
@@ -1,19 +1,34 @@
 # Import Flask class
-from flask import Flask
+from flask import Flask, request, render_template, make_response
+
+from linearRegression import linearRegression
 
 # Create Flask application instance
-app = Flask(__name__)
+app = Flask(__name__,template_folder="/home/prabhakar/PycharmProject/House_Price_Prediction/template")
+
 
 # Define a route (URL path) and a view function
-@app.route('/')  # Route for homepage
+@app.route('/model', methods=["POST"])
+def LinearRegression():
+    NumberOfRooms = request.form['NumberOfRooms']
+    NumberOfBedrooms = request.form['NumberOfBedrooms']
+    HouseAge = request.form['HouseAge']
+    AvgAreaIncome = request.form['AvgAreaIncome']
+    AreaPopulation = request.form['AreaPopulation']
+    predictedPrice = linearRegression(NumberOfRooms=int(NumberOfRooms), NumberOfBedrooms=int(NumberOfBedrooms), HouseAge=int(HouseAge),
+                                      AvgAreaIncome=int(AvgAreaIncome), AreaPopulation=int(AreaPopulation))
+    return make_response(predictedPrice)
 
+@app.route('/',methods=["GET"])
 def home():
-    return "Hello! 👋 Welcome to my first Flask App!"
+    return render_template("index.html")
 
-# Another route
+
+# Another route~e
 @app.route('/about')
 def about():
-    return "This is the About Page 🚀"
+    return render_template("about.html")
+
 
 # Run the app (start the Flask development server)
 if __name__ == "__main__":
